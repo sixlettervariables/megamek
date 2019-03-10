e2394be65986ca9668bd95dfc409cd1765cf52d1
Christopher Watford <christopher.watford@gmail.com>
2019-03-10 12:36:10 -0400

    Add accessibility resource bundle

---
+++ megamek/src/megamek/client/ui/swing/accessibility/AccessibilityMessages.java
@@ -0,0 +1,58 @@
+package megamek.client.ui.swing.accessibility;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import megamek.common.preference.PreferenceManager;
+import megamek.common.util.EncodeControl;
+
+public class AccessibilityMessages {
+    private static final String BUNDLE_NAME = "megamek.client.accessibility";//$NON-NLS-1$
+
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+            .getBundle(BUNDLE_NAME, PreferenceManager.getClientPreferences()
+                    .getLocale(), new EncodeControl());
+
+    private AccessibilityMessages() {
+        // All static class, should never be instantiated
+    }
+
+    /**
+     * Check to see if a given key has valid internationalized text.
+     * @param key
+     * @return
+     */
+    public static boolean keyExists(String key) {
+        return RESOURCE_BUNDLE.containsKey(key);
+    }
+
+    /**
+     * Retrieve the internationalized text for a given key.
+     *
+     * @param key
+     * @return
+     */
+    public static String getString(String key) {
+        try {
+            return RESOURCE_BUNDLE.getString(key);
+        } catch (MissingResourceException e) {
+            System.out.println("Missing i18n entry: " + key);
+            return '!' + key + '!';
+        }
+    }
+
+    /**
+     * Returns the formatted message for the given key in the resource bundle.
+     *
+     * @param key
+     *            the resource name
+     * @param args
+     *            the message arguments
+     * @return the string
+     */
+    public static String getString(String key, Object[] args) {
+        return MessageFormat.format(getString(key), args);
+    }
+
+}
